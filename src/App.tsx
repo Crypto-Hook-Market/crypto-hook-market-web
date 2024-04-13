43347b54133fc71bcc0314738ce164157835bb3e
iuesver <87600354+iuesver@users.noreply.github.com>
2024-04-13 18:35:15 +0900

    [feat] add initial  pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "@/router/index";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1>App</h1>
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
