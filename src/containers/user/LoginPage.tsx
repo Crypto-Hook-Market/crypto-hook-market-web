0f01ccf0d44fd66c17cd424fe76efbbdcfd4e8ce
iuesver <87600354+iuesver@users.noreply.github.com>
2024-04-21 15:17:49 +0900

    [feat] 카카오 로그인 리다이렉트 환경 변수 및 로직 추가

--- src/containers/user/LoginPage.tsx
+++ src/containers/user/LoginPage.tsx
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 const LoginPage = () => {
+  const location = useLocation();
+
+  const handleKakaoLogin = () => {
+    const kakaoURL = `${import.meta.env.VITE_KAKAO_LOGIN_URL}?client_id=${
+      import.meta.env.VITE_KAKAO_LOGIN_REST_API_KEY
+    }&redirect_uri=${
+      import.meta.env.VITE_KAKAO_LOGIN_REDIRECT_URI
+    }&response_type=code`;
+    window.location.href = kakaoURL;
+  };
+  useEffect(() => {
+    const accessToken = new URLSearchParams(location.search).get("code");
+    if (accessToken) {
+      console.log(accessToken);
+    }
+  }, [location]);
   return (
     <>
       <h1>로그인 페이지입니다.</h1>
-      <button onClick={() => {}}>카카오톡으로 로그인</button>
+      <button onClick={handleKakaoLogin}>카카오톡으로 로그인</button>
     </>
   );
 };
